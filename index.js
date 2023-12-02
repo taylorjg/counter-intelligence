82e6c4d79fc224dcedc4abe9f439bcc6e8bb6229
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-12-02 22:00:25 +0000

    Handle case here letterIndex + shift goes negative by wrapping around

--- index.js
+++ index.js
@@ -12,20 +12,33 @@ const counterIntelligence = (encoded) => {
   const letters = stringToLetters(encodedUpper);
   const lastLetter = last(letters);
   const lastLetterIndex = ALPHABET.indexOf(lastLetter);
-  const indexDiff = X_INDEX - lastLetterIndex;
+  const shift = X_INDEX - lastLetterIndex;
   const decodedLetters = letters.map((letter) => {
     const letterIndex = ALPHABET.indexOf(letter);
     if (letterIndex < 0) return letter;
-    const newIndex = (letterIndex + indexDiff) % ALPHABET.length;
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
+    const n = letterIndex + shift;
+    const d = ALPHABET.length;
+    const newIndex = ((n % d) + d) % d;
+
     return ALPHABET[newIndex];
   });
   return lettersToString(decodedLetters);
 };
 
-const main = () => {
-  const encoded = "NKRRU YCKKZNKGXZ D";
+const runExample = (encoded) => {
   const decoded = counterIntelligence(encoded);
   console.log({ encoded, decoded });
+};
+
+const main = () => {
+  runExample("Y");
+  runExample("BCD Y");
+  runExample("ANVNVKNA CX YRLT DY IDLLQRWR XW HXDA FJH QXVN OAXV FXAT, MJAURWP G");
+  runExample("NKRRU YCKKZNKGXZ D");
+  runExample("ngbk g toik jge :) d");
+  runExample("AAA Y");
 };
 
 main();
